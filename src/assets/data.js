53fb14ff7910011fb41fee429f3c61316f6bac81
HIMANSHUYELANE <yelanehimanshu@gmail.com>
2024-06-19 20:35:18 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question:"Which Device is required for internet Connection",
+        option0:"Pen Drive"  ,
+        option1:"Disk Drive",
+        option2:"Router",
+        option3:"Modem",
+        ans : 2
+    },
+    {
+        question:"Which Device is required for internet Connection",
+        option0:"Pen Drive"  ,
+        option1:"Disk Drive",
+        option2:"Router",
+        option3:"Modem",
+        ans : 1
+    },
+    {
+        question:"Which Device is required for internet Connection",
+        option0:"Pen Drive"  ,
+        option1:"Disk Drive",
+        option2:"Router",
+        option3:"Modem",
+        ans : 3
+    },
+    {
+        question:"Which Device is required for internet Connection",
+        option0:"Pen Drive"  ,
+        option1:"Disk Drive",
+        option2:"Router",
+        option3:"Modem",
+        ans : 0
+    },
+    {
+        question:"Which Device is required for internet Connection",
+        option0:"Pen Drive"  ,
+        option1:"Disk Drive",
+        option2:"Router",
+        option3:"Modem",
+        ans : 2
+    },
+]
